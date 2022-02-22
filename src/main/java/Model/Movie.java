6539c576d262df5d942a2aedeadf44cb1e33d46a
kaafj <kaafj@DESKTOP-LL89R6R>
2022-02-23 03:14:38 +0330

    addMovie

--- src/main/java/Model/Movie.java
+++ src/main/java/Model/Movie.java
@@ -1,19 +1,32 @@
 package Model;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Movie {
+    public Movie(int id, String name, String summary, String releaseDate, String director, ArrayList<String> writers,
+                 ArrayList<String> genres, ArrayList<Integer> cast, double imdbRate, long duration, int ageLimit){
+        Id = id;
+        Name = name;
+        Summary = summary;
+        ReleaseDate = releaseDate;
+        Director = director;
+        Writers = writers;
+        Genres = genres;
+        Cast = cast;
+        ImdbRate = imdbRate;
+        Duration = duration;
+        AgeLimit = ageLimit;
+    }
     public int Id;
     public String Name;
     public String Summary;
-    public Date ReleaseDate;
+    public String ReleaseDate;
     public String Director;
     public ArrayList<String> Writers;
     public ArrayList<String> Genres;
     public ArrayList<Integer> Cast;
-    public float ImdbRate;
-    public String Duration;
+    public double ImdbRate;
+    public long Duration;
     public int AgeLimit;
 
 }
