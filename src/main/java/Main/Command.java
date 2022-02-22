14f572c7833eca7aa41f5c155bb4023993fcca5e
pascalpc.com <afshinsharifian13@gmail.com>
2022-02-22 22:34:40 +0330

    add commands

--- src/main/java/Main/Command.java
+++ src/main/java/Main/Command.java
@@ -1,6 +1,36 @@
 package Main;
 
-public static class Command {
-    public static void proccess() {
+import Storage.Storage;
+
+public class Command {
+    Storage dataBase;
+    String command;
+    String JsonInput;
+    public Command(){
+        this.dataBase = new Storage();
     }
+
+    private static String CleanUpCommand(String commandLine) {
+        int whiteSpace = 0;
+        commandLine = commandLine.toLowerCase();
+        for (char c: commandLine.toCharArray())
+            if(c == ' ')
+                whiteSpace++;
+        return commandLine.substring(whiteSpace);
+    }
+
+    public void proccess(String commandLine) {
+        System.out.println(commandLine);
+
+        commandLine = CleanUpCommand(commandLine);
+        System.out.println(commandLine);
+        String[] commandParts = commandLine.split(" ", 2);
+
+        this.command = commandParts[0];
+        this.JsonInput = (commandParts.length > 1 ) ? commandParts[1] : null;
+    }
+
+
+
+
 }
